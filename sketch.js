a3878d3bc111d77ed6325624d167c0a89c101307
narenramakrishnan5 <74294122+narenramakrishnan5@users.noreply.github.com>
2021-02-12 09:28:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var canvas;
+var gameState,contestantCount,database,quiz,quizquestion,contestant,answer
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database=firebase.database()
+  quiz=new Quiz()
+  quiz.getState()
+  quiz.start()
+
+}
+
+
+function draw(){
+  background("pink");
+ if(contestantCount===4){
+   quiz.update(1)
+ }
+if(gameState===1){
+  clear()
+  quiz.play()
+}  
+}
